e8a300ec73c126a7e49d5af1deefa14c452e4ce5
MartijnVisser98 <m.visser.2@hotmail.com>
2019-01-13 22:59:46 +0100

    Added functional clock

--- src/parkeersimulator/model/Clock.java
+++ src/parkeersimulator/model/Clock.java
@@ -1,8 +1,12 @@
 package parkeersimulator.model;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Clock {
 
-    private int day = 0;
+    private int day = 1;
     private int hour = 0;
     private int minute = 0;
 
@@ -10,15 +14,15 @@ public class Clock {
     public void advanceTime(){
         // Advance the time by one minute.
         minute++;
-        while (minute > 59) {
+        if (minute > 59) {
             minute -= 60;
             hour++;
         }
-        while (hour > 23) {
+        if (hour > 23) {
             hour -= 24;
             day++;
         }
-        while (day > 6) {
+        if (day > 7) {
             day -= 7;
         }
 
@@ -37,6 +41,12 @@ public class Clock {
     public int getMinute()
     {
         return minute;
+    }
+
+    public String toString()
+    {
+        String dayOfWeek = DayOfWeek.of(day % 7).getDisplayName(TextStyle.FULL, Locale.getDefault());
+        return String.format("%s, day %o - %02d:%02d", dayOfWeek, getDay(), getHour(), getMinute());
     }
 }
 
--- src/parkeersimulator/view/TopBar.java
+++ src/parkeersimulator/view/TopBar.java
@@ -1,20 +1,51 @@
 package parkeersimulator.view;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class TopBar extends GridBagView {
+
+    private JLabel titleLabel;
+    private JLabel dateTimeLabel;
+    private JPanel spacer;
 
     public TopBar() {
         super();
         setGridWidth(2);
         setBackground(new Color(45, 52, 54));
 
+        setLayout(new GridBagLayout());
         setUIComponents();
     }
 
     private void setUIComponents()
     {
-        // @TODO Fill with the top bar items
+        titleLabel = new JLabel();
+        titleLabel.setFont(new Font("Dubai", Font.BOLD, 14));
+        titleLabel.setForeground(Color.white);
+        titleLabel.setText("Parkeersimulator");
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(0, 15, 0, 0);
+        add(titleLabel, constraints);
+
+        spacer = new JPanel();
+        spacer.setOpaque(false);
+        constraints = new GridBagConstraints();
+        constraints.weightx = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(spacer, constraints);
+
+        dateTimeLabel = new JLabel();
+        dateTimeLabel.setFont(new Font("Dubai Light", Font.BOLD, 14));
+        dateTimeLabel.setForeground(Color.white);
+        constraints = new GridBagConstraints();
+        constraints.insets = new Insets(0, 0, 0, 15);
+        add(dateTimeLabel, constraints);
+    }
+
+    public void setDateTimeLabelText(String text)
+    {
+        dateTimeLabel.setText(text);
     }
 
 }
